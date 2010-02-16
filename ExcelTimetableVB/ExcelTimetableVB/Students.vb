0435eb5090ef42de055f840c2293d027fd0bbeb5
Harrison Conlin <me@harrisony.com>
2010-02-16 23:20:18 +1100

    First revision of student list parser

---
+++ ExcelTimetableVB/ExcelTimetableVB/Students.vb
@@ -0,0 +1,23 @@
+﻿Imports Excel = Microsoft.Office.Interop.Excel
+Imports System.Text.RegularExpressions
+Imports System.IO
+Module Students
+    Public xlApp As Excel.Application
+    Public xlWorkBook As Excel.Workbook
+    Public xlRange As Excel.Range
+    Public xlWorksheet As Excel.Worksheet
+    Sub Main()
+        xlApp = New Excel.ApplicationClass
+        xlWorkBook = xlApp.Workbooks.Open("C:\Users\harrisony\Downloads\Current_Yr11_Student_Subjects.xls")
+        xlWorkSheet = xlWorkBook.Worksheets("Current_Yr11_Student_Subjects")
+        xlRange = xlWorksheet.UsedRange
+        Dim outputfile As StreamWriter = New StreamWriter("students.txt")
+        For row As Integer = 2 To (xlRange.Rows.Count) ' skip the titles row
+            For col As Integer = 1 To (xlRange.Columns.Count)
+                outputfile.Write(xlRange.Cells(row, col).Value & ",")
+            Next
+            outputfile.Write(vbNewLine)
+        Next
+        outputfile.Close()
+    End Sub
+End Module
