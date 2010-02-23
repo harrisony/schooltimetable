bdf2eaed55d98ab695d5feb2d191036c859ca030
Harrison Conlin <me@harrisony.com>
2010-02-23 23:00:22 +1100

    Deals with classes

--- ExcelTimetableVB/ExcelTimetableVB/Students.vb
+++ ExcelTimetableVB/ExcelTimetableVB/Students.vb
@@ -8,8 +8,23 @@ Module Students
         xlApp = New Excel.ApplicationClass
         xlWorkBook = xlApp.Workbooks.Open("C:\Users\harrisony\Downloads\Current_Yr11_Student_Subjects.xls")
         xlRange = xlWorkBook.Worksheets("Current_Yr11_Student_Subjects").UsedRange
-        Call all()
+        Call classes()
     End Sub
+    Sub classes()
+        Dim outputfile As StreamWriter = New StreamWriter("classes.txt")
+        Dim classes As New ArrayList
+        For row As Integer = 2 To (xlRange.Rows.Count) ' skip the titles row
+            Dim code As String = xlRange.Cells(row, 5).Value
+            Dim fullname As String = xlRange.Cells(row, 6).Value
+            If Not classes.Contains(code) Then
+                classes.Add(code)
+                outputfile.WriteLine(String.Format("{0},{1}", code, fullname))
+            End If
+
+        Next row
+        outputfile.Close()
+    End Sub
+
     Sub all()
         Dim outputfile As StreamWriter = New StreamWriter("students.txt")
         For row As Integer = 2 To (xlRange.Rows.Count) ' skip the titles row
@@ -25,4 +40,6 @@ Module Students
         Next
         outputfile.Close()
     End Sub
+
+
 End Module
