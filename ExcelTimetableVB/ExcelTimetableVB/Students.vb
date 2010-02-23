38c20ea5d4cc2b7aa91a58a75a392112aac48043
Harrison Conlin <me@harrisony.com>
2010-02-23 21:32:58 +1100

    Handle creating the CSV file better

--- ExcelTimetableVB/ExcelTimetableVB/Students.vb
+++ ExcelTimetableVB/ExcelTimetableVB/Students.vb
@@ -5,16 +5,19 @@ Module Students
     Public xlApp As Excel.Application
     Public xlWorkBook As Excel.Workbook
     Public xlRange As Excel.Range
-    Public xlWorksheet As Excel.Worksheet
     Sub Main()
         xlApp = New Excel.ApplicationClass
         xlWorkBook = xlApp.Workbooks.Open("C:\Users\harrisony\Downloads\Current_Yr11_Student_Subjects.xls")
-        xlWorkSheet = xlWorkBook.Worksheets("Current_Yr11_Student_Subjects")
-        xlRange = xlWorksheet.UsedRange
+        xlRange = xlWorkBook.Worksheets("Current_Yr11_Student_Subjects").UsedRange
         Dim outputfile As StreamWriter = New StreamWriter("students.txt")
         For row As Integer = 2 To (xlRange.Rows.Count) ' skip the titles row
             For col As Integer = 1 To (xlRange.Columns.Count)
-                outputfile.Write(xlRange.Cells(row, col).Value & ",")
+                If col = xlRange.Columns.Count Then
+                    ' last column we don't want a comma
+                    outputfile.Write(xlRange.Cells(row, col).Value)
+                Else
+                    outputfile.Write(xlRange.Cells(row, col).Value & ",")
+                End If
             Next
             outputfile.Write(vbNewLine)
         Next
