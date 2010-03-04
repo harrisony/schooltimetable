bbdcd03f024e1ffa20817d266f799b12547ec378
Harrison Conlin <me@harrisony.com>
2010-03-04 22:36:06 +1100

    Nice little viewer

--- ExcelTimetableVB/Viewer/Form1.Designer.vb
+++ ExcelTimetableVB/Viewer/Form1.Designer.vb
@@ -22,9 +22,62 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button
+        Me.TextBox1 = New System.Windows.Forms.TextBox
+        Me.ListBox1 = New System.Windows.Forms.ListBox
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(117, 224)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(117, 198)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(12, 58)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(414, 134)
+        Me.ListBox1.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(0, 13)
+        Me.Label1.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(438, 364)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
 
 End Class
--- ExcelTimetableVB/Viewer/Form1.vb
+++ ExcelTimetableVB/Viewer/Form1.vb
@@ -1,3 +1,31 @@
 ﻿Public Class Form1
 
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim db As SQLite.SQLiteConnection = New SQLite.SQLiteConnection("data source=students.db")
+        db.Open()
+        Using query As New SQLite.SQLiteCommand(db)
+            query.CommandText = "SELECT Class, Classes.Name from StudentsClasses JOIN Classes on (StudentsClasses.Class=Classes.Code) WHERE ComputerNumber = @compnumber"
+            query.Parameters.Add(New SQLite.SQLiteParameter("@compnumber", TextBox1.Text))
+            Dim response As SQLite.SQLiteDataReader = query.ExecuteReader()
+            ListBox1.Items.Clear()
+            If response.HasRows = True Then
+                While response.Read()
+                    ListBox1.Items.Add(String.Format("{0} {1}", response.GetValue(0), response.GetValue(1)))
+                End While
+            Else
+                ListBox1.Items.Add("No student found")
+            End If
+        End Using
+
+        Using query As New SQLite.SQLiteCommand(db)
+            Label1.Text = vbNullString
+            query.CommandText = "SELECT Surname, CName from Students WHERE ComputerNumber = @compnumber"
+            query.Parameters.Add(New SQLite.SQLiteParameter("@compnumber", TextBox1.Text))
+            Dim response As SQLite.SQLiteDataReader = query.ExecuteReader()
+            Label1.Text = String.Format("{0} {1}", response.GetValue(1), response.GetValue(0))
+        End Using
+
+
+    End Sub
+
 End Class
